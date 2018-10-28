1ca2ce6057355d0fac332e91f4e78cf7d62f5ec9
e-cloud <saintscott119@gmail.com>
2018-10-28 09:37:34 +0800

    chore: improve project template

--- test/util.js
+++ test/util.js
@@ -1,26 +1,20 @@
-const chai = require('chai');
-const chaiFiles = require('chai-files');
-const path = require('path');
-const fs = require('fs-extra');
-
-chai.use(chaiFiles);
-
-const {file} = chaiFiles;
+const path = require('path')
+const fs = require('fs-extra')
 
 function getSourceFile(subPath) {
-    return fs.readFile(path.resolve(__dirname, '__sources__', subPath), 'utf8');
+  return fs.readFile(path.resolve(__dirname, '__sources__', subPath), 'utf8')
 }
 
 function getBaselineFile(subPath) {
-    return fs.readFile(path.resolve(__dirname, '__baselines__', subPath), 'utf8');
+  return fs.readFile(path.resolve(__dirname, '__baselines__', subPath), 'utf8')
 }
 
 function getTemplate(subPath) {
-    return fs.readFile(path.resolve(__dirname, 'templates', subPath), 'utf8');
+  return fs.readFile(path.resolve(__dirname, 'templates', subPath), 'utf8')
 }
 
 module.exports = {
-    getSourceFile,
-    getBaselineFile,
-    getTemplate,
+  getSourceFile,
+  getBaselineFile,
+  getTemplate
 }
