238d529670dd66d8b633aca0388c5fb9d56648f7
Scott Ho <scott.he@quest.com>
2018-11-29 14:16:16 +0800

    fix(renderer): improve xml comment output format

--- lib/renderer/xml-renderer.ts
+++ lib/renderer/xml-renderer.ts
@@ -1,7 +1,7 @@
 import { generateBlockCommentContent, EOL, sharedInnerRender, xmlCommentRegexp } from '../util'
 
 export function render(source: string, template: string, cpRegExp: RegExp) {
-  const desiredComment = `<!--${generateBlockCommentContent(template, '-', false)}-->${EOL + EOL}`
+  const desiredComment = `<!--${generateBlockCommentContent(template, ' -', false)} -->${EOL + EOL}`
 
   return sharedInnerRender(desiredComment, source, xmlCommentRegexp, cpRegExp)
 }
