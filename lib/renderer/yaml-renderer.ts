5a828e431645eb792210719618502bfe88d59931
Scott Ho <scott.he@quest.com>
2018-12-14 14:52:35 +0800

    fix(yaml): trim the license template to prevent unintended empty line

--- lib/renderer/yaml-renderer.ts
+++ lib/renderer/yaml-renderer.ts
@@ -3,6 +3,7 @@ import { EOL, sharedInnerRender, yamlMultilineCommentRegexp } from '../util'
 export function render(source: string, template: string, cpRegExp: RegExp) {
   const desiredComment =
     template
+      .trim()
       .split('\n')
       .map(line => `# ${line}`.trimRight())
       .join(EOL) +
